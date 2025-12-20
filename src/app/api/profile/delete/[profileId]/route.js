447cfb107e11f20719695d9894bd8d2cd129bf51
Milad Akbari <milad.akbari.codevelop@gmail.com>
2025-12-20 23:36:23 +0330

    create DELETE Api

---
+++ src/app/api/profile/delete/[profileId]/route.js
@@ -0,0 +1,49 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import connectDb from "@utils/connectDB";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import Profile from "src/models/Profile";
+import User from "src/models/User";
+
+export async function DELETE(req, context) {
+  try {
+    await connectDb();
+    const id = context.params.profileId;
+
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        { error: "لطفا ابتدا وارد حساب کاربری خود شوید" },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+
+    const profile = await Profile.findOne({ _id: id });
+
+    if (!user._id.equals(profile.userId)) {
+      return NextResponse.json(
+        { error: "دسترسی شما به این آگهی محدود شده است" },
+        { status: 403 }
+      );
+    }
+    await Profile.deleteOne({ _id: id });
+    return NextResponse.json(
+      { message: "آگهی مورد نظر حذف شد" },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "مشکلی در سرور پیش آمده است" },
+      { status: 500 }
+    );
+  }
+}
