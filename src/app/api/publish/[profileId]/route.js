f0cf5a5104d011ab8ff10d544a208da45db81fd1
Milad Akbari <milad.akbari.codevelop@gmail.com>
2025-12-29 19:35:02 +0330

    created publish API

---
+++ src/app/api/publish/[profileId]/route.js
@@ -0,0 +1,43 @@
+import connectDb from "@utils/connectDB";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import Profile from "src/models/Profile";
+import User from "src/models/User";
+
+export async function PATCH(req, context) {
+  try {
+    await connectDb();
+    const id = context.params.profileId;
+    const session = await getServerSession(req);
+
+    if (!session) {
+      return NextResponse.json(
+        { error: "لطفا ابتدا وارد حساب کاربری خود شوید" },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+
+    if (user.role !== "ADMIN") {
+      return NextResponse.json({ error: " دسترسی محدود" }, { status: 403 });
+    }
+
+    const profile = await Profile.findOne({ _id: id });
+    profile.published = true;
+    profile.save();
+    return NextResponse.json({ message: "آگهی منتشر شد" }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "مشکلی در سرور پیش آمده است" },
+      { status: 500 }
+    );
+  }
+}
